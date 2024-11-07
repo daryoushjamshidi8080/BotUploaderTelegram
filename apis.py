12022fdf483a14d40d5d3276d34de454550ba95a
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2024-11-07 23:19:27 +0330

    add apis and The initial start of the bot

---
+++ apis.py
@@ -0,0 +1,4 @@
+bot_name= 'uploader'
+app_api_id = 19900466
+app_api_hash = "fc393ce306361ef2bb938ec8d2aa84fc"
+bot_api_hash = '7500784866:AAF2SlV6uzoFH3ZWnVAkRY_XetpPR1IfRC8'
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+#Librarys
+from pyrogram import Client,filters
+import apis
 
+
+
+
+
+# Bot initialization
+bot = Client(
+    apis.bot_name,
+    api_id=apis.app_api_id,
+    api_hash=apis.app_api_hash,
+    bot_token=apis.bot_api_hash
+    )
+
+
+@bot.on_message()
+async def main(cleint, message):
+    await message.reply_text("hello world")
+
+
+bot.run()
