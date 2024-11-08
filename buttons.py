4ba1d17a43ade072145c0dce670c4d412954a0c4
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2024-11-08 22:49:09 +0330

    Made basic buttons and connection test

---
+++ buttons.py
@@ -0,0 +1,22 @@
+from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton)
+
+
+
+class Buttons:
+    # menu button upload admin
+    def menu_upload(self):
+        upload_button =  ReplyKeyboardMarkup(
+            [
+                ['میخوام فیلم آپلود کنم']
+            ],resize_keyboard=True
+        )
+
+        return upload_button
+    # selection menu for upload movie
+    def menu_selection(self):
+        selection_button= InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton('تکی', callback_data='one_upload_movie'),InlineKeyboardButton('دسته جمعی', callback_data='collective_upload_movie')]
+            ]
+        )
+        return selection_button
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #Librarys
 from pyrogram import Client,filters
 import apis
-
-
+from buttons import Buttons
+from quesfuser import Response
 
 
 
@@ -14,10 +14,42 @@ bot = Client(
     bot_token=apis.bot_api_hash
     )
 
+#Object button
+buttons = Buttons()
+#Object Response
+response = Response()
 
 @bot.on_message()
 async def main(cleint, message):
-    await message.reply_text("hello world")
+
+    if message.text == 'میخوام فیلم آپلود کنم' and message.chat.id == 1655307519:
+        await message.reply_text('''
+            روش آپلودتو انتخاب کن 
+            تکی یا دست جمعی
+''', reply_markup=buttons.menu_selection())
+    
+    elif message.chat.id == 1655307519 :
+        await message.reply_text("میخوای چکار برات بکنم حاجی", reply_markup=buttons.menu_upload())
+
+
+
+
+@bot.on_callback_query()
+async def hande_callback_query(client, callback_query):
+
+
+    # upload one movie
+    if callback_query.data == 'one_upload_movie':
+        await callback_query.message.reply_text('اسم جدید را بفرستید')
+        resulte_response = await response.respons_text(bot, 1655307519)
+        print(resulte_response)
+
+    elif callback_query.data == 'collective_upload_movie':
+        await callback_query.message.reply_text('آپلود دسته جمعی')
+
+        
+
+
 
 
 bot.run()
